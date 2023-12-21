1a3ac256b6d11fff042cd9fdd80a6287b3dad1b6
md_shuaib <mshuaibin@gmail.com>
2023-12-21 21:14:56 +0530

    Initial commit search hidden

---
+++ 6.SearchIcon/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HiddenSearchIcon from './HiddenSearchIcon';
+
+function App() {
+  return (
+    <div className="App">
+    <HiddenSearchIcon />
+    </div>
+  );
+}
+
+export default App;
